58e1e92f3a78df438e1e73d2cdcbfe3f168d5e26
jtfrom9 <jun.tachikawa@gmail.com>
2020-08-21 10:58:36 +0900

    basic function

---
+++ Assets/Scripts/LoadPoints.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System;
+using System.Linq;
+using UnityEngine;
+
+public class LoadPoints : MonoBehaviour
+{
+    [SerializeField]
+    class Data
+    {
+        [SerializeField]
+        public List<Vector3> vertices = new List<Vector3>();
+    }
+
+    public string readText(string path)
+    {
+        string strStream = "";
+        try
+        {
+            using (StreamReader sr = new StreamReader(Application.dataPath + path))
+            {
+                strStream = sr.ReadToEnd();
+                sr.Close();
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.Message);
+        }
+
+        return strStream;
+    }
+
+    Mesh genMesh(Data d)
+    {
+        var mesh = new Mesh();
+        mesh.SetVertices(d.vertices);
+        mesh.SetIndices(Enumerable.Range(0, d.vertices.Count).Select(x => x).ToArray(),
+            MeshTopology.Points,
+            0);
+        return mesh;
+    }
+
+    void Start()
+    {
+        Debug.Log(Application.dataPath);
+        var json = readText("/vertex.json");
+        Debug.Log(json);
+        var d = JsonUtility.FromJson<Data>(json);
+        foreach(var p in d.vertices) {
+            Debug.Log(p);
+        }
+
+        var mf = GetComponent<MeshFilter>();
+        mf.sharedMesh = genMesh(d);
+    }
+}
---
+++ Assets/Shaders/PointCloud.shader
@@ -0,0 +1,43 @@
+﻿Shader "ZView/PointCloud"
+{
+   Properties {
+        _Color ("Color", Color) = (1, 1, 1, 1)
+        _PointSize ("Point Size", Range(1,30)) = 3
+    }
+    SubShader {
+        Tags { "RenderType"="Transparent" }
+        Cull Off ZWrite On Blend SrcAlpha OneMinusSrcAlpha
+        Pass {
+            CGPROGRAM
+
+            #pragma vertex vert
+            #pragma fragment frag
+            #include "UnityCG.cginc"
+            struct v2f {
+                float4 pos : SV_POSITION;
+                float3 worldPos : TEXCOORD0;
+                float size: PSIZE;
+            };
+            float4 _Color;
+            float _PointSize;
+
+            v2f vert(appdata_base v)
+            {
+                v2f o;
+				float3 n = UnityObjectToWorldNormal(v.normal);
+                o.pos = UnityObjectToClipPos (v.vertex);
+                o.worldPos = mul(unity_ObjectToWorld, v.vertex).xyz;
+                o.size = _PointSize;
+                return o;
+            }
+            half4 frag (v2f i) : COLOR
+            {
+            	float4 red  = float4(255.0/255,70.0/255,150.0/255,1);
+            	float4 blue = float4(90.0/255,90.0/255,250.0/255,1);
+                return lerp(red, blue, i.worldPos.y*0.2);
+            }
+            ENDCG
+        }
+    } 
+    FallBack Off
+}
